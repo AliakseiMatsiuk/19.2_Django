baf6ade254b8d0d61e6eb40b2656c53fe4993e76
Matsiuk Aliaksei <murryaa@gmai.com>
2023-05-09 19:23:54 +0300

    commit four

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from catalog.models import Product
 
 # Create your views here.
 def home(request):
-    return render(request, 'catalog/index.html')
+    context = {
+        'object_list': Product.objects.all(),
+        'title': 'Cписок продуктов'
+    }
+    return render(request, 'catalog/index.html', context)
+
+def product(request, pk):
+    product_item = Product.objects.get(pk=pk)
+    context = {
+        'object': product_item,
+        'title': product_item
+    }
+    return render(request, 'catalog/product.html', context)
 
 def contacts(request):
     return render(request, 'catalog/index2.html')
